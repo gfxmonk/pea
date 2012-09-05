0d5a940d6b5f446f6535845768acf90e9b24d7b4
Dan Tracy <dant@aweber.com>
2012-09-05 10:37:47 -0400

    formatter.py: Simplified formatter code structure

--- pea/formatter.py
+++ pea/formatter.py
@@ -1,5 +1,5 @@
-import os
-import sys
+from __future__ import unicode_literals, print_function
+
 import nose
 import functools
 import termstyle
@@ -10,68 +10,75 @@ success = 'passed'
 skip = 'skipped'
 line_length = 77
 
+
 class PeaFormatter(nose.plugins.Plugin):
-	name = 'pea'
-	score = 500
-	instance = None
-	_newtest = False
-	stream = None
-	
-	def __init__(self, *args):
-		self.enabled = False
-		type(self).instance = self
+    name = 'pea'
+    score = 500
+    instance = None
+    new_test = False
+    stream = None
 
-	def setOutputStream(self, stream):
-		type(self).stream = stream
+    def __init__(self):
+        self.enabled = False
+        PeaFormatter.instance = self
 
-	def configure(self, options, conf):
-		self.enabled = options.verbosity >= 2
-		if not self.enabled: return
-		color = getattr(options, 'color', True)
-		force_color = getattr(options, 'force_color', False)
-		if color:
-			try:
-				(termstyle.enable if force_color else termstyle.auto)()
-			except TypeError: # happens when stdout is closed
-				pass
-		else:
-			termstyle.disable()
+    def setOutputStream(self, stream):
+        PeaFormatter.stream = stream
 
-	def beforeTest(self, test):
-		type(self)._newtest = True
-	
-	def afterTest(self, test):
-		if self.enabled and self._newtest is False:
-			print >> self.stream, ""
+    def configure(self, options, conf):
+        self.enabled = options.verbosity >= 2
+        if not self.enabled:
+            return
+        color = getattr(options, 'color', True)
+        force_color = getattr(options, 'force_color', False)
+        if color:
+            try:
+                (termstyle.enable if force_color else termstyle.auto)()
+            except TypeError:  # happens when stdout is closed
+                pass
+        else:
+            termstyle.disable()
 
-	@classmethod
-	def with_formatting(cls, prefix, func):
-		def prn(s):
-			if cls.instance and cls.instance.enabled:
-				if cls._newtest:
-					print >> cls.stream, ""
-					cls._newtest = False
-				print >> cls.stream, s
+    def beforeTest(self, test):
+        PeaFormatter.new_test = True
 
-		@functools.wraps(func)
-		def nice_repr(obj):
-			return obj if isinstance(obj, str) else repr(obj)
+    def afterTest(self, test):
+        if self.enabled and not self.new_test:
+            print('', file=self.stream)
 
-		def _run(*a, **kw):
-			name = func.__name__.replace('_', ' ')
-			def desc(color):
-				desc = color("    %s %s" % (prefix, name))
-				if a:
-					desc += ' ' + color(termstyle.bold(' '.join(map(nice_repr,a))))
-				if kw:
-					desc += ' ' + ' '.join([color("%s=%s") % (k, termstyle.bold(repr(v))) for k,v in kw.items()])
-				return desc
-			try:
-				ret = func(*a, **kw)
-				prn(desc(termstyle.green))
-				return ret
-			except:
-				prn(desc(termstyle.red))
-				raise
-		return _run
+    @classmethod
+    def with_formatting(cls, prefix, func):
 
+        @functools.wraps(func)
+        def _run(*args, **kwargs):
+            name = func.__name__.replace('_', ' ')
+            color = termstyle.green
+            try:
+                return func(*args, **kwargs)
+            except:
+                color = termstyle.red
+                raise
+            finally:
+                if PeaFormatter.new_test:
+                    PeaFormatter.new_test = False
+                    print('', file=cls.stream)
+
+                output = describe(prefix, name, color, *args, **kwargs)
+                print(output, file=cls.stream)
+
+        return _run
+
+
+def describe(prefix, name, color, *args, **kwargs):
+    if args:
+        fmt_string = '\t{0} {1} {2}'
+        args = ' '.join(unicode(arg) for arg in args)
+        string = fmt_string.format(prefix, name, args)
+        return color(string)
+    elif kwargs:
+        fmt_string = '{0}={1}'
+        strings = [fmt_string.format(key, kwargs[key]) for key in kwargs]
+        kwargs = '{' + ', '.join(strings) + '}'
+        return color('\t' + ' '.join((prefix, name, kwargs)))
+    else:
+        return color('\t{0} {1}'.format(prefix, name))
