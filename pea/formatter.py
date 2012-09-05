514eefd12d78aa1ad3426064df723b2fe36a9f6e
Dan Tracy <dant@aweber.com>
2012-09-05 10:39:00 -0400

    formatter.py: Handle display of dictionaries separately

--- pea/formatter.py
+++ pea/formatter.py
@@ -4,10 +4,7 @@ import nose
 import functools
 import termstyle
 
-failure = 'FAILED'
-error = 'ERROR'
-success = 'passed'
-skip = 'skipped'
+
 line_length = 77
 
 
@@ -17,17 +14,14 @@ class PeaFormatter(nose.plugins.Plugin):
     instance = None
     new_test = False
     stream = None
-
-    def __init__(self):
-        self.enabled = False
-        PeaFormatter.instance = self
+    enabled = True
 
     def setOutputStream(self, stream):
         PeaFormatter.stream = stream
 
     def configure(self, options, conf):
-        self.enabled = options.verbosity >= 2
-        if not self.enabled:
+        PeaFormatter.enabled = options.verbosity >= 2
+        if not PeaFormatter.enabled:
             return
         color = getattr(options, 'color', True)
         force_color = getattr(options, 'force_color', False)
@@ -43,7 +37,7 @@ class PeaFormatter(nose.plugins.Plugin):
         PeaFormatter.new_test = True
 
     def afterTest(self, test):
-        if self.enabled and not self.new_test:
+        if PeaFormatter.enabled and not self.new_test:
             print('', file=self.stream)
 
     @classmethod
@@ -51,6 +45,9 @@ class PeaFormatter(nose.plugins.Plugin):
 
         @functools.wraps(func)
         def _run(*args, **kwargs):
+            if not cls.enabled:
+                return func(*args, **kwargs)
+
             name = func.__name__.replace('_', ' ')
             color = termstyle.green
             try:
@@ -70,15 +67,51 @@ class PeaFormatter(nose.plugins.Plugin):
 
 
 def describe(prefix, name, color, *args, **kwargs):
+    """Assemble the output for the test case by applying style and colors"""
     if args:
-        fmt_string = '\t{0} {1} {2}'
-        args = ' '.join(unicode(arg) for arg in args)
+        fmt_string = '\t{0} {1}: {2}'
+        rendered_args = ' '.join(_render(arg) for arg in args)
+        args = termstyle.bold(rendered_args)
         string = fmt_string.format(prefix, name, args)
         return color(string)
     elif kwargs:
         fmt_string = '{0}={1}'
-        strings = [fmt_string.format(key, kwargs[key]) for key in kwargs]
-        kwargs = '{' + ', '.join(strings) + '}'
-        return color('\t' + ' '.join((prefix, name, kwargs)))
+        rendered_kargs = [
+            fmt_string.format(key, _render(kwargs[key])) for key in kwargs
+        ]
+        kwargs = termstyle.bold(', ' .join(rendered_kargs))
+        return color('\t' + '{0} {1}: {2}'.format(prefix, name, kwargs))
     else:
         return color('\t{0} {1}'.format(prefix, name))
+
+
+def _render(arg):
+    """Will return a unicode representation of the argument if it is not a dict
+    otherwise it will render the dictionary differently
+
+    """
+    if _is_dict(arg):
+        return _render_dict(arg)
+    else:
+        return unicode(arg)
+
+
+def _is_dict(arg):
+    return isinstance(arg, dict)
+
+
+def _render_dict(kwargs):
+    """Provides an abridged look at a dictionary structure so the
+    argument display for the test dosen't span many lines and look cluttered.
+    It will only show the keys and the types of ther values for the first
+    level of the dictionary.  Nested dictionary structures will simply appear
+    like {key: <dict>}
+
+    The dictionary representation will look something like the following:
+
+    {key1: <type of key1s value>, key2: <type of key2s value>, ...}
+
+    """
+    fmt_string = "{0}: <{1}>"
+    args = [fmt_string.format(key, kwargs[key].__class__.__name__) for key in kwargs]
+    return '{' + ', '.join(args) + '}'
